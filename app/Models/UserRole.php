add986cdf2ae2e21142ae243c9d4725f05b9ab59
marchfish <379975597@qq.com>
2019-09-16 16:03:01 +0000

    添加攻击

--- app/Models/UserRole.php
+++ app/Models/UserRole.php
@@ -12,14 +12,28 @@ class UserRole
     public static function attackToEnemy($user_Role, $enemy)
     {
         $level_difference = $enemy->level - $user_Role->level;
-
         if ($level_difference > 0 && is_success($level_difference)) {
             return $enemy->name . '闪避了';
         }
 
-        if ($user_Role->attack - $enemy->defense) {
-
+        $user_hurt = $user_Role->attack - $enemy->defense;
+        if ($user_hurt <= 0) {
             return '无法破防';
+        }
+
+        $hurt_wave = mt_rand(0, round($user_hurt * 0.5));
+
+        $rand_num = mt_rand(0, 100);
+
+        if ($rand_num >= 50 || is_success($user_Role->crit)) {
+            $user_hurt += $hurt_wave;
+        }else {
+            $user_hurt -= $hurt_wave;
+        }
+
+        $enemy->hp -= $user_hurt;
+        if ($enemy->hp <= 0) {
+
         }
 
         return 0;
