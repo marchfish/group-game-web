11fd46bf1412ec9d5d32f46aac1cc754fd9a700d
marchfish <379975597@qq.com>
2019-09-18 16:13:15 +0000

    物品

--- app/Models/UserRole.php
+++ app/Models/UserRole.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Session;
 use InvalidArgumentException;
 use App\Models\Enemy;
 
@@ -12,6 +13,8 @@ class UserRole
 {
     public static function attackToEnemy($user_Role, $enemy)
     {
+        $user_role_id = Session::get('user.account.user_role_id');
+
         $level_difference = $enemy->level - $user_Role->level;
         if ($level_difference > 0 && is_success($level_difference)) {
             return $enemy->name . '闪避了';
@@ -34,6 +37,34 @@ class UserRole
 
         $enemy->hp -= $user_hurt;
         if ($enemy->hp <= 0) {
+            DB::table('user_role')
+                ->where('id', '=', $user_role_id)
+                ->update([
+                    'exp' => $enemy->exp,
+                    'coin' => $enemy->coin,
+                ])
+            ;
+
+            $certain_items = json_decode($enemy->certain_items);
+            $items = json_decode($enemy->items);
+
+            if (is_success($enemy->probability)) {
+                $count = mt_rand(0, count($items) - 1);
+                $win_item[0] = $items[$count];
+                $certain_items = array_merge($certain_items, obj2arr($win_item));
+                $result = [];
+                foreach($certain_items as $val){
+//                    $key = $val->id;
+
+//                    if(!isset($result[$key])){
+//                        $result[$key] = $val;
+//                    }else{
+//                        $result[$key]->num += $val->num;
+//                    }
+                }
+                return json_encode($certain_items);
+            }
+
             return $enemy->name . '被您击败了';
         }
 
