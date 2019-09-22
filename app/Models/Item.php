f282e044fee4040053fbd69010b52bf754703f6f
marchfish <379975597@qq.com>
2019-09-22 15:47:29 +0000

    使用物品

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -20,4 +20,46 @@ class Item
         ;
         return $items;
     }
+
+    // 使用药品
+    public static function useDrug($item)
+    {
+        $user_role_id = Session::get('user.account.user_role_id');
+
+        // 获取属性信息
+        $row = DB::query()
+            ->select([
+                'ur.*',
+            ])
+            ->from('user_role AS ur')
+            ->where('ur.id', '=', $user_role_id)
+            ->get()
+            ->first()
+        ;
+
+        $row->hp += $item->hp ?? 0;
+        $row->mp += $item->mp ?? 0;
+
+        DB::beginTransaction();
+
+        DB::table('user_role')
+            ->where('id', '=', $user_role_id)
+            ->update([
+                'hp' => $row->hp > $row->max_hp ? $row->max_hp : $row->hp,
+                'mp' => $row->mp > $row->max_mp ? $row->max_mp : $row->mp,
+            ])
+        ;
+
+        DB::table('user_knapsack')
+            ->where('user_role_id', '=', $user_role_id)
+            ->where('item_id', '=', $item->id)
+            ->update([
+                'item_num' => DB::raw('`item_num` - ' . 1),
+            ])
+        ;
+
+        DB::commit();
+
+        return '使用成功，血量 +' . $row->hp . '<br>' . '蓝量 +' . $row->mp;
+    }
 }
