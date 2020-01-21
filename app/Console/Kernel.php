ea96ef88d4d22f1897db1391674b1964649e28c8
marchfish <379975597@qq.com>
2020-01-21 08:19:18 +0000

    增加镇妖

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('order:finish')->everyMinute();
-        $schedule->command('order:refund')->everyMinute();
     }
 
     /**
